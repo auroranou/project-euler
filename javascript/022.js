56188bb46018a572039ff78661ed92ca0feb7b54
Aurora Nou <auroranou@gmail.com>
2017-06-06 20:42:34 -0400

    #22: name scores (what is readfilesync anyway)

---
+++ javascript/022.js
@@ -0,0 +1,64 @@
+/*
+  Using names.txt, a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order.
+  Then working out the alphabetical value for each name, multiply this value by its alphabetical positionin the list to obtain a name score.
+
+  For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53,
+  is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+  What is the total of all the name scores in the file?
+*/
+
+'use strict';
+let fs = require('fs');
+
+// Read in the names file
+let names = fs.readFileSync('../p022_names.txt', 'utf8');
+
+// Split the names into an alphabetized array
+names = names.replace(/['"]+/g, '')
+  .split(",")
+  .sort();
+
+let alphaMap = {
+  A: 1,
+  B: 2,
+  C: 3,
+  D: 4,
+  E: 5,
+  F: 6,
+  G: 7,
+  H: 8,
+  I: 9,
+  J: 10,
+  K: 11,
+  L: 12,
+  M: 13,
+  N: 14,
+  O: 15,
+  P: 16,
+  Q: 17,
+  R: 18,
+  S: 19,
+  T: 20,
+  U: 21,
+  V: 22,
+  W: 23,
+  X: 24,
+  Y: 25,
+  Z: 26
+};
+
+names = names.map((name, i, a) => {
+  // Get the sum of letter values for each name
+  let nameScore = name.split('').reduce((prev, curr) => {
+    return +alphaMap[curr] + prev;
+  }, 0);
+  // Multiply each name score by its position in the array (one-indexed)
+  return nameScore * (i + 1);
+}).reduce((prev, curr) => {
+  // Add everything up
+  return prev + curr;
+}, 0);
+
+console.log(names);
+// Time: 111ms
